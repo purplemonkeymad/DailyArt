3331cc428804c994b58225b8de374c24573c685d
david @ work <david@brettle.org.uk>
2020-05-13 13:30:02 +0100

    add a check for imgur "removed" image.

--- DailyArt/public/Get-SubredditImages.ps1
+++ DailyArt/public/Get-SubredditImages.ps1
@@ -117,6 +117,24 @@ function Get-SubredditImages {
                 if ($PSCmdlet.ShouldProcess($_.url , "Download $($_.name), with title $($_.title) to $outPath")){
                     Invoke-WebRequest $_.url -OutFile $outPath -UseBasicParsing
                     $OutputFile = Get-Item $outPath -ErrorAction SilentlyContinue
+
+                    #imgur does not 404 missing images but replaces them with the same image
+                    # we check if the size and has match the removed image.
+
+                    # the removed image is 503 bytes and has a sha 256 hash of
+                    # 9B5936F4006146E4E1E9025B474C02863C0B5614132AD40DB4B925A10E8BFBB9
+                    $BadHash = '9B5936F4006146E4E1E9025B474C02863C0B5614132AD40DB4B925A10E8BFBB9'
+
+                    if ($OutputFile.Length -eq 503) {
+                        # only do a hash if the length matches
+                        $FileHash = Get-FileHash $OutputFile.FullName | ForEach-Object Hash
+                        if ($BadHash -eq $FileHash){
+                            Remove-Item $$OutputFile -ErrorAction SilentlyContinue
+                            # clear variable containing file so that further tests fail.
+                            $OutputFile = $null
+                        }
+                    }
+
                     if ($SaveMetaData -and $OutputFile){
                         $jsonout =  ( Join-path $OutputFile.Directory $OutputFile.basename ) +'.json'
                         $_ | ConvertTo-Json -Depth 20 | Set-Content $jsonout
